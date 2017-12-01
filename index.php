dba84fe825067814fcdbdf9ed752ac80f6f2a772
jesus lopez rodriguez <34141467+morulopez@users.noreply.github.com>
2017-12-01 16:32:37 +0100

    index.php
    en este archivo esta la pagina inicial del gestor de entrada con los inputs de usuario y contraseña

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+	include_once("funciones.php");
+?>
+<html>
+	<Head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="entradas.css">
+	</Head>
+	<body>
+		<div class="container">
+			<form action="" method="POST">
+				 <div class="form-group">
+					<label for="usuario_form">Usuario</label>
+					<input type="text" class="form-control" name="usuario" id="usuario_form">
+				</div>
+				<div class="form-group">
+					<label for="password_form">Contraseña</label>
+					<input type="password" class="form-control" name="password" id="password_form">
+				</div>
+					<input type="submit" name="boton" class="btn btn-primary btn-lg btn-block">
+			</form>	
+				</div>
+
+	</body>
+</html>
+<?php
+	$seguridad = new seguridad;
+	if(isset($_POST["usuario"])){
+		$seguridad->password($_POST["boton"],$_POST["usuario"],$_POST["password"]);
+		header("location:gestion.php");
+	}
+	
+	
+	
+	
+
+?>
